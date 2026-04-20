edaa54701a2602fc97a6858ca9ff80867bd9f1ed
Vikaskush1070 <155914486+Vikaskush1070@users.noreply.github.com>
2026-04-20 09:52:01 +0000

    again

--- .devcontainer/01_basics/second.js
+++ .devcontainer/01_basics/second.js
@@ -2,3 +2,10 @@ const score = 400
 const balance = new Number(100)
 
 console.log(balance);
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,6,8));
