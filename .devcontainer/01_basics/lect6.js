6f9bdf606825c128b81e6efbd18ccd3a2e00f059
Vikaskush1070 <155914486+Vikaskush1070@users.noreply.github.com>
2026-04-21 18:27:34 +0000

    some code

---
+++ .devcontainer/01_basics/lect6.js
@@ -0,0 +1,41 @@
+const regularUser = {
+
+    email:"vikas@gmail.com",
+    fullName: {
+        userfullName: {
+            firstName: " vikas",
+            lastName: "kumar"
+        }
+      
+    }
+
+}
+
+// console.log(regularUser.fullName)
+
+const obj1 = { 1: "a" , 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 ={obj1, obj2}
+const obj3 = Object.assign({},obj1 , obj2)
+const obj4 = {...obj1, ...obj2}
+console.log(obj3);
+console.log(obj4);
+
+
+const users = [
+    {
+    id : 1,
+    name: " vikas"
+},
+{
+    id : 2,
+    email: "vikas@gmail.com"
+}
+]
+console.log(users);
+
+users[1].email
+console.log(regularUser);
+
+console.log(Object.keys(regularUser));
---
+++ .devcontainer/01_basics/lect7.js
@@ -0,0 +1,9 @@
+const course = {
+    coursename: "Hindi",
+    price: "99",
+    courseInstructor: "vikas"
+}
+const {courseInstructor:instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
