e20c8b932bd556195eb9ffef50a026d90066c2a5
Vikaskush1070 <155914486+Vikaskush1070@users.noreply.github.com>
2026-04-24 08:47:46 +0000

    switch

---
+++ .devcontainer/02_basics/switch.js
@@ -0,0 +1,27 @@
+const months = 4
+
+switch (months) {
+    case 1:
+        console.log("Jab");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+
+    case 3:
+        console.log("Mae");
+        break;
+
+    case 4:
+        console.log("Apri");
+        break;
+
+    default:
+        console.log("valid months");
+        break;
+
+
+
+
+}
+
